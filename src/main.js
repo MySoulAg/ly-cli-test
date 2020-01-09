118444521a1ebf4441b7d670929e9af0d69d8b22
MySoulAg <643953991@qq.com>
2020-01-09 14:01:58 +0800

    懒加载

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import store from './store/index.js'
+import VueLazyload from 'vue-lazyload'
 import VueWechatTitle from 'vue-wechat-title' //设置页面标题
 
 Vue.use(VueWechatTitle)
+
+//src下的assets目录下的图片
+Vue.use(VueLazyload,{
+  error:require('./assets/imgs/error.jpg'),
+  loading:require('./assets/imgs/loading.gif')
+})
 
 router.beforeEach((to, from, next) => {
   store.commit("isShow", true)
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,21 +5,36 @@
     <button @click="add">+1</button>
     <p>父组件的数据：{{ num }}</p>
     <Dialog class="fegrgreg" ref="dialog"></Dialog>
-    <!-- <Child :num="num"></Child> -->
+    <Child :num="num"></Child>
+    <img v-lazy="item" alt="" v-for="(item, index) in imgList" :key="index" />
   </article>
 </template>
 <script>
 import Dialog from '@/components/Dialog.vue'
-// import Child from '@/components/Child.vue'
+import Child from '@/components/Child.vue'
 export default {
   name: 'home',
   components: {
-    Dialog
-    // Child
+    Dialog,
+    Child
   },
   data() {
     return {
-      num: 0
+      num: 0,
+      imgList: [
+        'https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2080745074,2793209448&fm=26&gp=0.jpg',
+        'https://dss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3381573685,1866477444&fm=26&gp=0.jpg',
+        'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2229864841,4232235061&fm=26&gp=0.jpg',
+        'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3209370120,2008812818&fm=26&gp=0.jpg',
+        'https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2462146637,4274174245&fm=26&gp=0.jpg',
+        'https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1719063513,2559625643&fm=26&gp=0.jpg',
+        'https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2080745074,2793209448&fm=26&gp=0.jpg',
+        'https://dss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3381573685,1866477444&fm=26&gp=0.jpg',
+        'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2229864841,4232235061&fm=26&gp=0.jpg',
+        'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3209370120,2008812818&fm=26&gp=0.jpg',
+        'https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2462146637,4274174245&fm=26&gp=0.jpg',
+        'https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1719063513,2559625643&fm=26&gp=0.jpg'
+      ]
     }
   },
 
@@ -36,8 +51,8 @@ export default {
 </script>
 <style lang="less" scoped>
 article {
-  
-    
-  
+  img {
+    width: 50%;
+  }
 }
 </style>
