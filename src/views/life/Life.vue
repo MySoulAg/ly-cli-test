2cb4dcd86eaf69a972b8aa9a3b014d6edd9c124c
li.yin <li.yin@ebiz-fooge.com>
2020-01-16 08:48:27 +0800

    CSS3动画

--- src/views/life/Life.vue
+++ src/views/life/Life.vue
@@ -2,6 +2,10 @@
   <article>
     生活
     <my-tab></my-tab>
+    <!-- <h1 class="animation">动画Animation</h1> -->
+    <div class="img animation">
+      <img src="@/assets/imgs/open.png" alt="" />
+    </div>
   </article>
 </template>
 <script>
@@ -9,7 +13,7 @@ import Tab from '@/components/Tab.vue'
 export default {
   name: 'life',
   components: {
-    'my-tab': Tab,
+    'my-tab': Tab
   },
   data() {
     return {}
@@ -19,5 +23,56 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+.img {
+  width: 70px;
+  height: 70px;
+  margin: 100px auto;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+.animation {
+  animation: jello 0.5s;
+  animation-iteration-count: infinite;
+}
 
+@keyframes jello {
+  11.1% {
+    transform: scale(1, 1);
+  }
+
+  22.2% {
+    transform: scale(1.04, 1.04);
+  }
+
+  33.3% {
+    transform: scale(1.08, 1.08);
+  }
+
+  44.4% {
+    transform: scale(1.10, 1.10);
+  }
+
+  55.5% {
+    transform: scale(1.10, 1.10);
+  }
+
+  66.6% {
+    transform: scale(1.10, 1.10);
+  }
+
+  77.7% {
+    transform: scale(1.08, 1.08);
+  }
+
+  88.8% {
+    transform: scale(1.04, 1.04);
+  }
+
+  88.8% {
+    transform: scale(1, 1);
+  }
+}
 </style>
+1
