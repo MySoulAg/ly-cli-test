95c72c1e688ced896b146411d284285dab94b3cc
li.yin <li.yin@ebiz-fooge.com>
2020-01-19 10:52:43 +0800

    实时检测输入框中的name是否被占用

--- src/views/life/Life.vue
+++ src/views/life/Life.vue
@@ -2,6 +2,16 @@
   <article>
     生活
     <my-tab></my-tab>
+    <div class="input">
+      <input
+        type="text"
+        v-model="msg"
+        @keydown.delete="deletDown"
+        @keyup.delete="deletUp"
+        @input="inputMsg"
+      />
+      <span>{{info}}</span>
+    </div>
   </article>
 </template>
 <script>
@@ -12,12 +22,53 @@ export default {
     'my-tab': Tab
   },
   data() {
-    return {}
+    return {
+      msg: '',
+      info: '',
+      isDeletUp: true
+    }
   },
 
-  methods: {}
+  methods: {
+    inputMsg() {
+      window.clearTimeout(window.timeOut)
+      window.timeOut = window.setTimeout(() => {
+        if (this.msg != '' && this.isDeletUp) {
+          this.info = '正在校验姓名....'
+          window.timeOut1 = window.setTimeout(() => {
+            this.checkName(this.msg)
+            window.clearTimeout(window.timeOut1)
+          }, 1000)
+        } else {
+          this.info = ''
+        }
+      }, 500)
+    },
+
+    deletUp() {
+      this.isDeletUp = true
+    },
+
+    deletDown() {
+      this.isDeletUp = false
+    },
+
+    checkName(name) {
+      if (name == 'admin') {
+        this.info = '该用户名已经被注册！'
+      } else {
+        this.info = '该用户名可用'
+      }
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
+.input {
+  padding: 20px;
 
+  input {
+    margin-top: 2px;
+  }
+}
 </style>
